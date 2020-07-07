dfd342503c553d455a6833370d33ebcbae5b8b69
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-07 13:52:32 +0300

    created login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+  isLoading: boolean;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private api: ApiService) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  login() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.isLoading = true;
+
+    this.api.login('Users/login', this.loginForm.value).subscribe(
+      res => {
+        localStorage.setItem('token', res.id);
+        localStorage.setItem('ttl', res.ttl);
+        this.router.navigate(['/home']);
+        this.isLoading = false;
+      },
+      err => {
+        this.isLoading = false;
+        // this.errorHandler.handleError(err);
+      }
+    );
+  }
+
+}
