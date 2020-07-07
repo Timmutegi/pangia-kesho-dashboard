c9e2e3c9639ef94c012ca4d9e1460152261935fd
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-07 13:53:10 +0300

    Added API endpoint

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl = 'https://mbima.bluewaveinsurance.co:4434/api/PolicyHolders';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(endpoint: string, data: JSON) {
+    return this.http.post<any>(this.baseUrl + endpoint, data);
+  }
+
+  get(endpoint: string) {
+    return this.http.get<any>(this.baseUrl + endpoint);
+  }
 }
