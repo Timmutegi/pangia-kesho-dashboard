9eeac28f5829dd9ccad407dbb836b3c889654efe
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-10 10:29:00 +0300

    Added details route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailsComponent } from './components/details/details.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'details/:ID', component: DetailsComponent }
 ];
 
 @NgModule({
