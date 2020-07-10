9be7d5f114b6be4babf1ec1221bb58040dcf4f19
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-10 10:27:18 +0300

    Added data from API

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { Router } from '@angular/router';
+
+export interface Data {
+  createdAt: Date;
+}
+
+const ELEMENT_DATA: Data[] = [];
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +17,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  isLoading = true;
+  minDate: Date;
+  maxDate: Date;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['index', 'fullname', 'gender', 'church', 'phone', 'ID', 'email', 'cover', 'dob', 'createdAt'];
+  filterForm = new FormGroup({
+    fromDate: new FormControl(),
+    toDate: new FormControl(),
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  get fromDate() {
+    const fromDate = new Date(this.filterForm.get('fromDate').value);
+    return fromDate;
+  }
+  get toDate() {
+    const toDate = new Date(this.filterForm.get('toDate').value);
+    toDate.setDate(toDate.getDate() + 1);
+    return toDate;
   }
 
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+
+  constructor(private api: ApiService, private router: Router) { }
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.getPolicyHolders();
+    this.maxDate = new Date();
+    this.minDate = new Date(2020, 1, 7);
+    this.dataSource.filterPredicate = (data, filter) => {
+      if (this.fromDate && this.toDate) {
+        return data.createdAt >= this.fromDate && data.createdAt <= this.toDate;
+      }
+      return true;
+    };
+  }
+
+  getPolicyHolders() {
+    this.api.get('/PolicyHolders').subscribe(
+      res => {
+        res.forEach((element: { createdAt: string | number | Date; }) => {
+          element.createdAt = new Date(element.createdAt);
+        });
+        console.log(res);
+        this.isLoading = false;
+        this.dataSource.data = res;
+      }
+    );
+  }
+
+  applyFilter1() {
+    this.dataSource.filter = '' + Math.random();
+  }
+
+  reset() {
+    this.filterForm.reset();
+    this.dataSource.filter = '';
+  }
+
+  getRecord(ID: string) {
+    console.log(ID);
+    this.router.navigate([`details/${ID}`]);
+  }
 }
