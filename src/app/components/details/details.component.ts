c71e3514debd4da31bc840685f316fc3fd8d51f3
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-10 10:24:24 +0300

    Created details component

---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+  ID: string;
+  policyHolder: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
+
+  ngOnInit() {
+    this.ID = this.activatedRoute.snapshot.params.ID;
+    this.api.get('/PolicyHolders/' + this.ID).subscribe(
+      res => {
+        console.log(res);
+        this.policyHolder = res;
+      }
+    );
+
+  }
+
+}
