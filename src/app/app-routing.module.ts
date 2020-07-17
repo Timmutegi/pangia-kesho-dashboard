600f8ffb826f3e782169b5d82543761738537c99
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-17 11:33:07 +0300

    Added route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DetailsComponent } from './components/details/details.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'details/:ID', component: DetailsComponent }
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'details/:ID', component: DetailsComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
